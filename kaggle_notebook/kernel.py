91535ee26027ccae662b1e98ca3a6c39a5c71e93
zzazzz <ziyad.azzufari@gmail.com>
2026-03-17 17:03:34 +0700

    Update kernel.py

--- kaggle_notebook/kernel.py
+++ kaggle_notebook/kernel.py
@@ -11,12 +11,12 @@ subprocess.run(["pip", "install", "-q", "wandb", "huggingface_hub",
 wandb.login(key="WANDB_KEY_PLACEHOLDER")
 login(token="HF_KEY_PLACEHOLDER")
 
-shutil.copy("/kaggle/input/trashnet-training-script/model_training_resnet.py", "model_training_resnet.py")
-shutil.copy("/kaggle/input/trashnet-training-script/model_training_swin.py", "model_training_swin.py")
-shutil.copy("/kaggle/input/trashnet-training-script/validate_model_resnet.py", "validate_model_resnet.py")
-shutil.copy("/kaggle/input/trashnet-training-script/validate_model_swin.py", "validate_model_swin.py")
-shutil.copy("/kaggle/input/trashnet-training-script/requirements.txt", "requirements.txt")
-shutil.copytree("/kaggle/input/trashnet-data/data", "data")
+shutil.copy("/kaggle/input/datasets/ziyadmuhammad/trashnet-training-script/model_training_resnet.py", "model_training_resnet.py")
+shutil.copy("/kaggle/input/datasets/ziyadmuhammad/trashnet-training-script/model_training_swin.py", "model_training_swin.py")
+shutil.copy("/kaggle/input/datasets/ziyadmuhammad/trashnet-training-script/validate_model_resnet.py", "validate_model_resnet.py")
+shutil.copy("/kaggle/input/datasets/ziyadmuhammad/trashnet-training-script/validate_model_swin.py", "validate_model_swin.py")
+shutil.copy("/kaggle/input/datasets/ziyadmuhammad/trashnet-training-script/requirements.txt", "requirements.txt")
+shutil.copytree("/kaggle/input/datasets/ziyadmuhammad/trashnet-data/data", "data")
 
 # Training ResNet50
 print("\n========== TRAINING RESNET50 ==========")
